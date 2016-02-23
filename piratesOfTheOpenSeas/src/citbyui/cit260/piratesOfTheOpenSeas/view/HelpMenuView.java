cce17ecf8f2e69ae3396aaeadceecddf5f96cd4b
Liz <Liz@DESKTOP-IKAS1FJ>
2016-02-23 13:22:18 -0800

    Help menu complete

--- piratesOfTheOpenSeas/src/citbyui/cit260/piratesOfTheOpenSeas/view/HelpMenuView.java
+++ piratesOfTheOpenSeas/src/citbyui/cit260/piratesOfTheOpenSeas/view/HelpMenuView.java
@@ -24,6 +24,7 @@ public class HelpMenuView {
                 +"\nB - What is the objective of the game?"
                 +"\nN - How to navigate"
                 +"\nC - How to select a crew"
+                +"\nL - How to stock your ship"
                 +"\nF - How a pirate can attack, negotiate or run from other pirates, navy or merchants"
                 +"\nM - How to use the map"
                 +"\nQ - Quit"
@@ -82,6 +83,9 @@ public class HelpMenuView {
             case "C": //display the help menu
                 this.selectCrew();
                 break;
+            case "L": //how to stock the ship
+                this.stockShip();
+                break;
             case "F": //save the current game
                 this.displayEnemyTactics();
                 break;
@@ -97,25 +101,46 @@ public class HelpMenuView {
 
     private void displayGameObjective() {
          System.out.println("*** The objective of the game is to find the hidden"
-                 + "treasure while avoiding capture, or worse, from your enemy. "
-                 + "The journey will be long and dangerous.  You will "
-                 + "have to risk life or death if you want to find the treasure "
-                 + "of all treasures. ***");
+                 + "\ntreasure while avoiding capture, or worse, from your enemy. "
+                 + "\nThe journey will be long and dangerous.  You will "
+                 + "\nhave to risk life or death if you want to find the treasure "
+                 + "\nof all treasures. ***");
     }
 
     private void howToNavigate() {
-        System.out.println("*** howToNavigate function called ***");
+        System.out.println("*** You will given a range of coordinates to choose from."
+                + "\neach coordinate will represent a different scene i.e. island,"
+                + "\nport, open sea etc...  ***");
     }
 
     private void selectCrew() {
-         System.out.println("*** selectCrew function called ***");
+         System.out.println("*** If you chose a small ship you may have between "
+                 + "\n2 - 9 crew members to choose from.  If you chose a large"
+                 + "\nship you may have 8 - 19 crew members. ***");
     }
 
     private void displayEnemyTactics() {
-         System.out.println("*** displayEnemyTactics function called ***");
+         System.out.println("*** You will be given the option to run, fight or "
+                 + "\nnegotiate.  If you choose to fight you will be given a range "
+                 + "\nof numbers to choose from that will determine whether they "
+                 + "\nwill win or lose the fight. If you choose to negotiate you will "
+                 + "\nchoose a number between 1 and 10, 1 = not willing to sacrifice "
+                 + "\nanything, and 10 = giving everything. If the enemy rejects the "
+                 + "\nnegotiated offer 3 times then you will have to choose whether "
+                 + "\nto run or fight.  If you choose to run "
+                 + "\nyou will be given the option to enter new coordinates.***");
     }
 
     private void displayMap() {
-         System.out.println("*** displayMap function called ***");
+         System.out.println("*** There will be clues in the map that will help"
+                 + "\nto navigate to where you need to go. ***");
+    }
+
+    private void stockShip() {
+        System.out.println("*** You will be prompted to enter the number of pounds"
+                + "\nof food you would like to get per person per day.  You will "
+                + "\nbe able to choose your weapons and the size of barrels you want"
+                + "\nfor water and rum. Obviously you will not be able to have"
+                + "\nas much if you chose the smaller ship.***");
     }
 }
