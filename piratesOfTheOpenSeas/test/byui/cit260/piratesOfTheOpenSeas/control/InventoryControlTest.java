32aa81515992a6f144e1a52b2443c6bac56c335e
Liz <Liz@DESKTOP-IKAS1FJ>
2016-02-27 12:07:49 -0800

    Inventory Control

--- piratesOfTheOpenSeas/test/byui/cit260/piratesOfTheOpenSeas/control/InventoryControlTest.java
+++ piratesOfTheOpenSeas/test/byui/cit260/piratesOfTheOpenSeas/control/InventoryControlTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Roderick
+ * @author Roland
  */
 public class InventoryControlTest {
     
@@ -32,59 +32,286 @@ public class InventoryControlTest {
      */
     @Test
     public void testCalcBarrelVolume() {
+        
         System.out.println("calcBarrelVolume");
-        double height = 0.0;
-        double diameter = 0.0;
-        double expResult = 0.0;
-        double result = InventoryControl.calcBarrelVolume(height, diameter);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        /************************
+         *  Test case 1
+         ***********************/
+        System.out.println("tTest case #1");
+        
+        // input values for test case 1
+        double height = 24.0;
+        double diameter = 24.0;
+        
+        InventoryControl instance = new InventoryControl();
+        
+        double expResult = 10857.34; // expected value 
+        
+        // create instance of InvrntoryControl class
+        double result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
+        
+        
+        /************************
+         *  Test case 2
+         ***********************/
+        System.out.println("tTest case #2");
+        
+        // input values for test case 2
+        height = 60.0;
+        diameter = 20.0;
+        
+        
+        expResult = -1; // expected value 
+        
+        // create instance of InvrntoryControl class
+        result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
+        
+        
+        /************************
+         *  Test case 3
+         ***********************/
+        System.out.println("tTest case #3");
+        
+        // input values for test case 2
+        height = 30.0;
+        diameter = -6.0;
+        
+        
+        expResult = -1; // expected value 
+        
+        // create instance of InvrntoryControl class
+        result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
+        
+        /************************
+         *  Test case 4
+         ***********************/
+        System.out.println("tTest case #4");
+        
+        // input values for test case 4
+        height = 10.0;
+        diameter = 40.0;
+        
+        
+        expResult = -1; // expected value 
+        
+        // create instance of InvrntoryControl class
+        result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
+        
+        
+        /************************
+         *  Test case 5
+         ***********************/
+        System.out.println("tTest case #5");
+        
+        // input values for test case 5
+        height = 12.0;
+        diameter = 16.0;
+        
+        
+        expResult = 2412.74; // expected value 
+        
+        // create instance of InvrntoryControl class
+        result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
+        
+        
+        /************************
+         *  Test case 6
+         ***********************/
+        System.out.println("tTest case #6");
+        
+        // input values for test case 6
+        height = 48.0;
+        diameter = 12.0;
+        
+        
+        expResult = 5428.67; // expected value 
+        
+        // create instance of InvrntoryControl class
+        result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
+        
+        
+        /************************
+         *  Test case 7
+         ***********************/
+        System.out.println("tTest case #7");
+        
+        // input values for test case 7
+        height = 20.0;
+        diameter = 36.0;
+        
+        
+        expResult = 20357.52; // expected value 
+        
+        // create instance of InvrntoryControl class
+        result = instance.calcBarrelVolume(height, diameter);
+        
+        assertEquals(expResult, result, 0.01);
     }
 
     /**
      * Test of calcFoodWanted method, of class InventoryControl.
      */
     @Test
-    public void testCalcFoodWanted_3args_1() {
-        System.out.println("calcFoodWanted");
-        int noOfPeople = 0;
-        int poundsPerDay = 0;
-        int noOfDays = 0;
-        int expResult = 0;
-        int result = InventoryControl.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of displayInventoryControl method, of class InventoryControl.
-     */
-    @Test
-    public void testDisplayInventoryControl() {
-        System.out.println("displayInventoryControl");
+    public void testCalcFoodWanted() {
+        
+        /****************
+         * Test case #1
+         ****************/
+        
+        System.out.println("\tTest case #1");
+        
+        //input values to test case 1
+        int noOfPeople = 25;
+        int poundsPerDay = 3;
+        int noOfDays = 30;
+        
+        
         InventoryControl instance = new InventoryControl();
-        instance.displayInventoryControl();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of calcFoodWanted method, of class InventoryControl.
-     */
-    @Test
-    public void testCalcFoodWanted_3args_2() {
-        System.out.println("calcFoodWanted");
-        double noOfPeople = 0.0;
-        double poundsPerDay = 0.0;
-        double noOfDays = 0.0;
-        InventoryControl instance = new InventoryControl();
-        double expResult = 0.0;
-        double result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        int expResult = 2250;//expected output returned value
+        
+        //call function to run test
+        int result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+    
+    
+        /****************
+         * Test case #2
+         ****************/
+        
+        System.out.println("\tTest case #2");
+        
+        //input values to test case 2
+        noOfPeople = 60;
+        poundsPerDay = 0;
+        noOfDays = 10;
+        
+        
+        expResult = -1;//expected output returned value
+        
+        //call function to run test
+        result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+        
+        
+        /****************
+         * Test case #3
+         ****************/
+        
+        System.out.println("\tTest case #3");
+        
+        //input values to test case 3
+        noOfPeople = 20;
+        poundsPerDay = 11;
+        noOfDays = 12;
+        
+        
+        expResult = -1;//expected output returned value
+        
+        //call function to run test
+        result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+        
+        /****************
+         * Test case #4
+         ****************/
+        
+        System.out.println("\tTest case #4");
+        
+        //input values to test case 4
+        noOfPeople = 5;
+        poundsPerDay = 4;
+        noOfDays = 0;
+        
+        
+        expResult = -1;//expected output returned value
+        
+        //call function to run test
+        result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+        
+        
+        /****************
+         * Test case #5
+         ****************/
+        
+        System.out.println("\tTest case #5");
+        
+        //input values to test case 5
+        noOfPeople = 50;
+        poundsPerDay = 3;
+        noOfDays = 8;
+        
+        
+        expResult = 1200;//expected output returned value
+        
+        //call function to run test
+        result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+        
+        
+        /****************
+         * Test case #6
+         ****************/
+        
+        System.out.println("\tTest case #6");
+        
+        //input values to test case 6
+        noOfPeople = 12;
+        poundsPerDay = 1;
+        noOfDays = 21;
+        
+        
+        expResult = 252;//expected output returned value
+        
+        //call function to run test
+        result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+        
+        
+        /****************
+         * Test case #7
+         ****************/
+        
+        System.out.println("\tTest case #7");
+        
+        //input values to test case 7
+        noOfPeople = 20;
+        poundsPerDay = 3;
+        noOfDays = 50;
+        
+        
+        expResult = 3000;//expected output returned value
+        
+        //call function to run test
+        result = instance.calcFoodWanted(noOfPeople, poundsPerDay, noOfDays);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0);
+    } 
     
 }
